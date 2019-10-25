a70420b01e223de23890fca2fb0d98afff0f351f
Topcuogullari <trtopcmu@TRSEUISTMAC033.local>
2019-10-25 17:33:59 +0300

    update RestApi.py and README

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ Behave Library as BDD tool. As result, it is so easy to use for both Api and Web
 ### Install
 
 Run "pip install -r requirements.txt". Then go to under api or web path and run "behave". Here we go!
+
+### For More
+
+https://splinter.readthedocs.io/en/latest/
+https://behave.readthedocs.io/en/latest/
+https://pypi.org/project/requests/2.7.0/
+https://pypi.org/project/simplejson/
--- api/features/steps/RestApi.py
+++ api/features/steps/RestApi.py
@@ -2,29 +2,36 @@ import requests
 import simplejson as json
 from behave import *
 
-url = obj = res = None
+
+class User:
+    def __init__(self):
+        self.name = ""
+        self.email = ""
+        self.status = ""
+
+
+user = User()
 
 
 @given('the RestApi url is hit on "{num}" page')
 def step_impl(context, num):
-    global obj
-    global res
     r = requests.get("https://reqres.in/api/users?page=" + str(num))
-    res = r.status_code
-    json_data = r.text
-    obj = json.loads(json_data)
+    user.status = r.status_code
+    obj = json.loads(r.text)  # Text response is being converted to Json format.
+    user.name = obj["data"][0]["first_name"]
+    user.email = obj["data"][0]["email"]
 
 
 @when('the response status should be "{num}"')
 def step_impl(context, num):
-    assert str(res) == str(num), 'Response should be "%s", not "%s!"' % (str(num), str(res))
+    assert str(user.status) == str(num), 'Response should be "%s", not "%s"!' % (str(num), str(user.status))
 
 
 @then('the first user name should be "{name}"')
 def step_impl(context, name):
-    assert name == obj["data"][0]["first_name"], 'Name should be "%s", not "%s"!' % (name, obj["data"][0]["first_name"])
+    assert name == user.name, 'Name should be "%s", not "%s"!' % (name, user.name)
 
 
 @then('the first user email should be "{email}"')
 def step_impl(context, email):
-    assert email == obj["data"][0]["email"], 'Email should be "%s", not "%s"!' % (email, obj["data"][0]["email"])
+    assert email == user.email, 'Email should be "%s", not "%s"!' % (email, user.email)
